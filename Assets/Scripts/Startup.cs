13f9e365ce582575b23260df0b68bd7dd2705280
Colink02 <colinkinzel@gmail.com>
2019-05-22 00:52:01 -0500

    updates

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -1,28 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Startup : MonoBehaviour {
     public string objectName;
     public GameObject Camera;
+    public Button play, exit;
     void Awake() {
         objectName = gameObject.name;
         Camera = GameObject.Find("MenuCamera");
         Debug.Log("objectName is " + objectName);
     }
+    void Start()
+    {
+    }
 	// Use this for initialization
-	void OnMouseUp () {
-        if(objectName.Equals("Play"))
+	void listenerCall () {
+        if(objectName.Equals("New Game"))
         {
             Debug.Log("Hey Load Scene...");
             SceneManager.LoadScene(1);
-        } else if(objectName.Equals("Settings"))
+        } else if(objectName.Equals("Exit"))
         {
-            Camera.transform.Rotate(new Vector3(0, 180, 0)); 
-        } else if(objectName.Equals("SavedFiles"))
-        {
-
+            Application.Quit();
         }
 	}
     void OnMouseEnter () {
@@ -35,4 +38,86 @@ public class Startup : MonoBehaviour {
 	void Update () {
 		
 	}
+}
+[RequireComponent(typeof(Button))]
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    Text txt;
+    Color baseColor;
+    Button btn;
+    bool interactableDelay;
+
+    void Start()
+    {
+        txt = GetComponentInChildren<Text>();
+        baseColor = txt.color;
+        btn = gameObject.GetComponent<Button>();
+        interactableDelay = btn.interactable;
+    }
+
+    void Update()
+    {
+        if (btn.interactable != interactableDelay)
+        {
+            if (btn.interactable)
+            {
+                txt.color = baseColor * btn.colors.normalColor * btn.colors.colorMultiplier;
+            }
+            else
+            {
+                txt.color = baseColor * btn.colors.disabledColor * btn.colors.colorMultiplier;
+            }
+        }
+        interactableDelay = btn.interactable;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (btn.interactable)
+        {
+            txt.color = baseColor * btn.colors.highlightedColor * btn.colors.colorMultiplier;
+        }
+        else
+        {
+            txt.color = baseColor * btn.colors.disabledColor * btn.colors.colorMultiplier;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (btn.interactable)
+        {
+            txt.color = baseColor * btn.colors.pressedColor * btn.colors.colorMultiplier;
+        }
+        else
+        {
+            txt.color = baseColor * btn.colors.disabledColor * btn.colors.colorMultiplier;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (btn.interactable)
+        {
+            txt.color = baseColor * btn.colors.highlightedColor * btn.colors.colorMultiplier;
+        }
+        else
+        {
+            txt.color = baseColor * btn.colors.disabledColor * btn.colors.colorMultiplier;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (btn.interactable)
+        {
+            txt.color = baseColor * btn.colors.normalColor * btn.colors.colorMultiplier;
+        }
+        else
+        {
+            txt.color = baseColor * btn.colors.disabledColor * btn.colors.colorMultiplier;
+        }
+    }
+
 }
